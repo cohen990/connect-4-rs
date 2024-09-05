1d5d36d31b4b7b941119fbf53f4650e74761c12a
Samantha Cohen <samanthacohen294@gmail.com>
2024-09-05 22:40:17 +0100

    implement player switching

--- src/game.rs
+++ src/game.rs
@@ -18,7 +18,17 @@ impl Game {
     }
 
     pub fn play_on_column(self, column: usize) -> Self {
-        self
+        let new_player = match self.current == self.player_one {
+            true => self.player_two.to_owned(),
+            false => self.player_one.to_owned(),
+        };
+        Self {
+            player_one: self.player_one,
+            player_two: self.player_two,
+            current: new_player,
+            is_complete: false,
+            winner: None,
+        }
     }
 }
 
@@ -30,5 +40,15 @@ mod tests {
     fn game_initialises_with_the_player_one_playing_first() {
         let game = Game::init("x", "y");
         assert_eq!(game.player_one, game.current)
+    }
+
+    #[test]
+    fn player_one_and_player_two_take_turns() {
+        let mut game = Game::init("x", "y");
+        assert_eq!(game.player_one, game.current);
+        game = game.play_on_column(1);
+        assert_eq!(game.player_two, game.current);
+        game = game.play_on_column(1);
+        assert_eq!(game.player_one, game.current);
     }
 }
