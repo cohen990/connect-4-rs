a41a45b194bb5b9c6b383538549c96cea84e415a
Samantha Cohen <samanthacohen294@gmail.com>
2024-09-05 22:34:12 +0100

    adds the first unit test

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,7 @@
 pub struct Game {
     player_one: String,
     player_two: String,
+    current: String,
     pub is_complete: bool,
     pub winner: Option<String>,
 }
@@ -10,6 +11,7 @@ impl Game {
         Game {
             player_one: player_one.to_owned(),
             player_two: player_two.to_owned(),
+            current: player_one.to_owned(),
             is_complete: false,
             winner: None,
         }
@@ -17,5 +19,16 @@ impl Game {
 
     pub fn play_on_column(self, column: usize) -> Self {
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Game;
+
+    #[test]
+    fn game_initialises_with_the_player_one_playing_first() {
+        let game = Game::init("x", "y");
+        assert_eq!(game.player_one, game.current)
     }
 }
